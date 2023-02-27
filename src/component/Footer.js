a2d73cdc4cc060e43d29247f7813d571ede61add
MathurJi <anileshjjssacademy84@gmail.com>
2023-02-27 14:38:42 +0530

    add the footer and header

---
+++ src/component/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <>
+    <div>
+        <footer className="text-center text-white fixed-bottom" style={{backgroundColor:' #21081a'}}>
+  <div className="container p-4"></div>
+
+  <div className="text-center p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
+    <a className="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+</footer>
+    </div>
+    </>
+  )
+}
